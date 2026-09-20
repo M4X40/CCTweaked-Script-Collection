b7f7f755738a2981bf3666fe973c0b361e2c1502
M4X40 <m4x4o@proton.me>
2026-09-20 16:12:22 -0400

    actually include warp

--- CC_AP_Testing.lua
+++ CC_AP_Testing.lua
@@ -1,9 +1,15 @@
 core = peripheral.find("end_automata")
 
 success, msg = core.savePoint("test1")
+print(success)
+print(msg)
 
 local i = 1
 repeat
   turtle.forward()
   i = i + 1
 until i = 20
+
+success, msg = core.warpToPoint("test1")
+print(success)
+print(msg)
